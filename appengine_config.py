abe4cd2cb33e60eb94a65b70e1ccd99057a20eea
Graeme Pyle <graeme@graemep.co.za>
2013-05-09 17:33:50 +0200

    Allow data admin access

--- appengine_config.py
+++ appengine_config.py
@@ -82,3 +82,13 @@ def gcb_configure_dev_server_if_running():
 
 gcb_init_third_party()
 gcb_configure_dev_server_if_running()
+
+#########################################
+# Remote_API Authentication configuration.
+#
+# See google/appengine/ext/remote_api/handler.py for more information.
+# For datastore_admin datastore copy, you should set the source appid
+# value.  'HTTP_X_APPENGINE_INBOUND_APPID', ['trusted source appid here']
+#
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
+    'HTTP_X_APPENGINE_INBOUND_APPID', ['ami-mooc-pilot'])
